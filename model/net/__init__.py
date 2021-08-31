3b52c962475bf0415e33cf705f8c764c337a72d0
bubble0000 <971770426@qq.com>
2021-08-31 17:00:26 +0800

    add mobilenetv2

--- model/net/__init__.py
+++ model/net/__init__.py
@@ -1,6 +1,7 @@
 from model.net.resNet import *
 from model.net.resNext import *
 from model.net.seNet import *
+from model.net.mobilenetv2 import mobilenet_v2
 
 __all__=['ResNet','resnet34', 'resnet50', 'resnet101', 'resnet152','SENet', 'se_resnet_34', 'se_resnet_50',
-        'resnext101','resnext50','resnext152']
+        'resnext101','resnext50','resnext152','mobilenet_v2']
