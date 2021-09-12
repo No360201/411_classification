aef6767ce8a77a7cd5e3e5e2b6ddbe4e123f5e0f
bubble0000 <971770426@qq.com>
2021-09-12 16:52:23 +0800

    add shufflenetv1

--- model/__init__.py
+++ model/__init__.py
@@ -2,7 +2,8 @@ from model.net import *
 from model.net_test import *
 
 __all__=['ResNet','resnet34', 'resnet50', 'resnet101', 'resnet152','SENet','se_resnet_34', 'se_resnet_50'
-           ,'resnext101','resnext50','resnext152','resnet34_fpn', 'resnet50_fpn','mobilenet_v2','densenet121','densenet161','densenet169','densenet201']
+           ,'resnext101','resnext50','resnext152','resnet34_fpn', 'resnet50_fpn','mobilenet_v2','densenet121','densenet161','densenet169','densenet201',
+         'shufflenet_v1']
 
 def build_model(config):
     return globals()[config['arch']](**config['kwargs'])
