022c39ef266802ba25f169bc8f3bd729486c101e
bubble0000 <971770426@qq.com>
2021-11-10 11:15:12 +0800

    add vgg

--- model/__init__.py
+++ model/__init__.py
@@ -3,7 +3,7 @@ from model.net_test import *
 
 __all__=['ResNet','resnet34', 'resnet50', 'resnet101', 'resnet152','SENet','se_resnet_34', 'se_resnet_50'
            ,'resnext101','resnext50','resnext152','resnet34_fpn', 'resnet50_fpn','mobilenet_v2','densenet121','densenet161','densenet169','densenet201',
-         'shufflenet_v1']
+         'shufflenet_v1','vgg16','vgg19']
 
 def build_model(config):
     return globals()[config['arch']](**config['kwargs'])
--- model/net/__init__.py
+++ model/net/__init__.py
@@ -4,7 +4,8 @@ from model.net.seNet import *
 from model.net.mobilenetv2 import mobilenet_v2
 from model.net.densenet import *
 from model.net.shufflenetv1 import shufflenet_v1
+from model.net.vgg import *
 
 __all__=['ResNet','resnet34', 'resnet50', 'resnet101', 'resnet152','SENet', 'se_resnet_34', 'se_resnet_50',
         'resnext101','resnext50','resnext152','mobilenet_v2','densenet121','densenet161','densenet169','densenet201',
-         'shufflenet_v1']
+         'shufflenet_v1','vgg16','vgg19']
